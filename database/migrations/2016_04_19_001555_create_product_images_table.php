634611bb2dbd5f3dfd1dc522ba969a39b422cbd6
Deveron Reniers <def-lol-lol@hotmail.com>
2016-04-19 02:36:06 +0200

    admin panel

---
+++ database/migrations/2016_04_19_001555_create_product_images_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_images', function (Blueprint $table) {
+            $table->increments('id')->index();
+            $table->integer('user_id')->index();
+            $table->string('path', 20)->index();
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('password_resets');
+    }
+}
